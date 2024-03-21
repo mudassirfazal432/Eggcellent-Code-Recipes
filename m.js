7c58795b1957b8b4b757c6dd8789f6727162bbd5
Mudassir Fazal <162458958+mudassirfazal432@users.noreply.github.com>
2024-03-22 04:22:57 +0500

    Add files via upload

---
+++ m.js
@@ -0,0 +1,8 @@
+// Function recipe ka name halfFryEgg //
+function halfFryEgg() {
+    var cooked = 1 + 1.5 + 2;
+    // egg + butter + salt
+    console.log(cooked);
+}
+halfFryEgg();
+// Function ko call karna //
---
+++ m.ts
@@ -0,0 +1,7 @@
+// Function recipe name halfFryEgg //
+function halfFryEgg()
+{let cooked = 1 + 1.5 + 2 
+// egg + butter + salt //
+console.log(cooked)}
+halfFryEgg()
+// Calling the function //
